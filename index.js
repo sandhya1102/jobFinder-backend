06f006a777b3458135e033128f1b0e9e24e7e895
Sandhya <150221789+sandhya1102@users.noreply.github.com>
2025-07-16 21:14:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: "https://job-finder-frontend-chi.vercel.app",
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
 };
